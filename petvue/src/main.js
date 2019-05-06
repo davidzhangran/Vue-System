3a50eaf8a3e12890c993f876f51b8fdc1b444b45
zhangran <675807219@qq.com>
2019-05-06 09:03:43 +0800

    vue新增主界面

--- petvue/src/main.js
+++ petvue/src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 引入进来就可以了，自动运行
+import l from "./_gloabal";
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
