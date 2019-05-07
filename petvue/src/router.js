9b3c0c6c7df59f87d59a38bf1edbe1ca6b8151f1
yaohuiling <1602573810@qq.com>
2019-05-07 18:16:04 +0800

    宠物修改，删除

--- petvue/src/router.js
+++ petvue/src/router.js
@@ -5,10 +5,7 @@ import UserSystem from "./views/userSystem";
 import UserStore from "./views/userStore";
 import Pet from "./components/pet/pet";
 import Storefront from "./components/storefront/storefront"
-<<<<<<< HEAD
-=======
 
->>>>>>> e56a80d13a2f3e6c31608c1accf1d19a101c16d4
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -27,12 +24,7 @@ export default new Router({
       path: '/userStore',
       name: 'userStore',
       component: UserStore,
-<<<<<<< HEAD
-      children:[
-        {
-=======
       children: [{
->>>>>>> e56a80d13a2f3e6c31608c1accf1d19a101c16d4
         path: 'Pet',
         name: 'Pet',
         component: Pet,
@@ -41,11 +33,7 @@ export default new Router({
         path: 'storefront',
         name: 'storefront',
         component: Storefront
-<<<<<<< HEAD
       }]
-=======
-      },]
->>>>>>> e56a80d13a2f3e6c31608c1accf1d19a101c16d4
     }
   ]
 })
--- petvue/src/store/pet.js
+++ petvue/src/store/pet.js
@@ -4,15 +4,45 @@ export default {
    //命名空间，防止不同的状态重名
     namespaced:true,
       state: {
-       
+        currentPage: 1,
+        eachPage: 10,
+        maxPage: 0,
+        total: 0,
+        pets: [],
+        param:{}
       },
       mutations: {//同步方法
-        
+        getPetsByPage(state, payload) {
+          Object.assign(state, payload);//合并操作，相当于上一种
+          console.log(state);
+      },
+        setEachPage: (state, eachPage) => state.eachPage = eachPage,
+        setCurrentPage: (state, currentPage) => state.currentPage = currentPage,
       },
     actions: {
         async addPetAsync(context,playlod) {
-            const data = await petService.addPet(playlod)
-            console.log(data);
+            const data = await petService.addPet(playlod);
+            context.dispatch("getPetsByPageAsync");
+        },
+        //分页
+        async getPetsByPageAsync(context,{type,text}={}) {
+          const { currentPage, eachPage } = context.state;
+          // if(!type&text){
+
+          // }
+          const data = await petService.getPetsByPageSer({ currentPage,eachPage,type,text});//拿到数据，通过mutations触发数据更新
+          console.log(data)
+          context.commit("getPetsByPage", data);//通过commit触发getStudentsByPage
+        },
+        //删除宠物
+        async removePetAsync(context,payload){
+          const data=await petService.removePet({_id:payload});
+          context.dispatch("getPetsByPageAsync");
+        },
+        //修改
+        async updatePetAsync(context,payload){
+          const data=await petService.updatePet(payload);
+          context.dispatch("getPetsByPageAsync");
         },
     
     },
